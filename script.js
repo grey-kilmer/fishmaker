5210912f6fce4866430812784235bce60c9236ae
grey-kilmer <119373392+grey-kilmer@users.noreply.github.com>
2025-06-21 02:50:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var name="";
+var bones=[];
+var importTextures=[];
+var textures=[];
+var animationStyle=0;
+var animationDefintions=[];
+var speed=0;
+var health=0;
+var maxDepth=0;
+var minDepth=0;
+var prefersBottomFeeding=false;
+var abilities=[];
+
+var currentBone, currentSplinePoint, 
 function addBone(){
   
 }
