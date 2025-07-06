33caf4fb6787f62b061a5b1b71d11320fc921588
grey-kilmer <119373392+grey-kilmer@users.noreply.github.com>
2025-07-06 04:29:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -198,11 +198,11 @@ function drawBone(){
     var angleToChild=Math.atan2(childBone["y"]-bone["y"],childBone["x"]-bone["x"]);
     canvas.beginPath();
     canvas.moveTo(10*Math.cos(angleToChild+Math.PI/2)+bone["x"],10*Math.sin(angleToChild+Math.PI/2)+bone["y"]);
-    canvas.lineTo(childBone["x"],childBone["y"]);
+    canvas.lineTo(childBone["x"]+10*Math.cos(angleToChild+Math.PI),childBone["y"]+10*Math.sin(angleToChild+Math.PI));
     canvas.stroke();
     canvas.beginPath();
     canvas.moveTo(10*Math.cos(angleToChild-Math.PI/2)+bone["x"],10*Math.sin(angleToChild-Math.PI/2)+bone["y"]);
-    canvas.lineTo(childBone["x"],childBone["y"]);
+    canvas.lineTo(childBone["x"]+10*Math.cos(angleToChild+Math.PI),childBone["y"]+10*Math.sin(angleToChild+Math.PI));
     canvas.stroke();
   }
 }
