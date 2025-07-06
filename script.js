ee9f86de68eb4064bae268061c248acbb031a748
grey-kilmer <119373392+grey-kilmer@users.noreply.github.com>
2025-07-06 05:47:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -250,6 +250,7 @@ canvas.onmousedown=function(event){
 }
 canvas.onmouseup=function(event){
   console.log(event);
+  console.log(isDraggingBone);
   if (isDraggingBone){
     var x=2*parseInt(event.offsetX);
     var y=2*parseInt(event.offsetY);
