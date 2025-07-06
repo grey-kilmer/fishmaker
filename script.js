d4b98e1ae1d350e02c45e8c47601df0fcbf38991
grey-kilmer <119373392+grey-kilmer@users.noreply.github.com>
2025-07-06 01:09:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -85,8 +85,8 @@ function updateBoneListPearl(bone, list){
   option.value=bone["id"];
   option.className="boneChoice";
   prefix="";
-  for (var i=0;i<getGenerationOf(bone);i++){prefix+="-";}
-  prefix+=">";
+  for (var i=0;i<getGenerationOf(bone);i++){prefix+="║";}
+  prefix+="╟";
   option.innerHTML=prefix+bone["name"]
   list.appendChild(li);
   selects=document.querySelectorAll(".boneSelector");
